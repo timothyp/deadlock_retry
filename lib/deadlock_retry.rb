e6814941142e0d6a4f962e06fab3e27552cd517d
Joey Geiger <joey.geiger@irco.com>
2018-07-10 10:28:38 -0600

    Remove innodb logging
    This logging gives a full innodb dump of the DB and all active
    transactions. We hit too many deadlocks for this to be useful so it's
    getting removed.
    
    [#158950085]

--- lib/deadlock_retry.rb
+++ lib/deadlock_retry.rb
@@ -4,7 +4,6 @@ module DeadlockRetry
   DEFAULT_MAXIMUM_RETRIES_ON_DEADLOCK = 3
 
   mattr_accessor :maximum_retries_on_deadlock
-  mattr_accessor :innodb_status_cmd
 
   DEADLOCK_ERROR_MESSAGES = [
     "Deadlock found when trying to get lock",
@@ -18,8 +17,6 @@ module DeadlockRetry
   def transaction(*objects, &block)
     retry_count = 0
 
-    check_innodb_status_available
-
     begin
       super
     rescue ActiveRecord::StatementInvalid, ActiveRecord::Deadlocked => error
@@ -27,7 +24,6 @@ module DeadlockRetry
       if DEADLOCK_ERROR_MESSAGES.any? { |msg| error.message =~ /#{Regexp.escape(msg)}/ }
         retries_exhausted = retry_count >= DeadlockRetry.maximum_retries_on_deadlock
         logger.info "Deadlock detected on attempt #{retry_count + 1}. Max retries: #{DeadlockRetry.maximum_retries_on_deadlock}, so #{'not ' if retries_exhausted}restarting transaction. Exception: #{error.to_s}"
-        log_innodb_status if DeadlockRetry.innodb_status_cmd
         raise if retries_exhausted
         retry_count += 1
         exponential_pause(retry_count)
@@ -53,49 +49,6 @@ module DeadlockRetry
   def in_nested_transaction?
     # open_transactions was added in 2.2's connection pooling changes.
     connection.open_transactions != 0
-  end
-
-  def show_innodb_status
-    self.connection.select_one(DeadlockRetry.innodb_status_cmd)['Status']
-  end
-
-  # Should we try to log innodb status -- if we don't have permission to,
-  # we actually break in-flight transactions, silently (!)
-  def check_innodb_status_available
-    return unless DeadlockRetry.innodb_status_cmd == nil
-
-    if self.connection.adapter_name.downcase.include?('mysql')
-      begin
-        mysql_version = self.connection.select_rows('show variables like \'version\'')[0][1]
-        cmd = if mysql_version < '5.5'
-          'show innodb status'
-        else
-          'show engine innodb status'
-        end
-        self.connection.select_value(cmd)
-        DeadlockRetry.innodb_status_cmd = cmd
-      rescue
-        logger.info "Cannot log innodb status: #{$!.message}"
-        DeadlockRetry.innodb_status_cmd = false
-      end
-    else
-      DeadlockRetry.innodb_status_cmd = false
-    end
-  end
-
-  def log_innodb_status
-    # Showing the innodb status is the only way to get visiblity into why the
-    # transaction deadlocked.  Log it, along with a prefix including an id to
-    # enable easy extraction of the resulting status dump from the log.
-    lines = show_innodb_status
-    deadlock_id = SecureRandom.hex(4)
-    logger.info "(INNODB #{deadlock_id}) Status follows:"
-    lines.each_line do |line|
-      logger.info "(INNODB #{deadlock_id}) " + line
-    end
-  rescue => e
-    # Access denied, ignore
-    logger.info "Cannot log innodb status: #{e.message}"
   end
 end
 
